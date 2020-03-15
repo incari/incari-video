5b32ac9e8956b402b989fc7590d456173944e717
Martin Racana <ing.racana@gmail.com>
2020-03-15 13:38:54 +0100

    register logic

---
+++ src/containers/Home.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { connect } from "react-redux";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import useInitialState from "../hooks/useInitialState";
+
+import "../assets/styles/App.scss";
+
+const Home = ({ myList, trends, originals }) => {
+  return (
+    <>
+      <Search />
+
+      {/* 
+      {Object.keys(initialState).map(category => {
+        if (initialState[category].length) {
+          return (
+            <Categories key={category} title={category}>
+              <Carousel>
+                {initialState[category].map(item => (
+                  <CarouselItem key={item.id} {...item} />
+                ))}
+              </Carousel>
+            </Categories>
+          );
+        }
+      })}
+     */}
+
+      {myList.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            {myList.map(item => (
+              <CarouselItem key={item.id} {...item} isList />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {trends.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {originals.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
---
+++ src/reducers/index.js
@@ -0,0 +1,40 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_FAVORITE":
+      return {
+        ...state,
+        myList: [
+          ...state.myList,
+          ...(state.myList.map(i => i.id).indexOf(action.payload.id) === -1
+            ? [action.payload]
+            : [])
+        ]
+      };
+
+    case "DELETE_FAVORITE":
+      return {
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload)
+      };
+    default:
+      return state;
+
+    case "LOGIN_REQUEST":
+      return {
+        ...state,
+        user: action.payload
+      };
+    case "LOGOUT_REQUEST":
+      return {
+        ...state,
+        user: action.payload
+      };
+    case "REGISTER_REQUEST":
+      return {
+        ...state,
+        user: action.payload
+      };
+  }
+};
+
+export default reducer;
